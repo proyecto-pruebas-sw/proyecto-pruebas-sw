8c88e2a7d018d92167f6b714bc0b387b6d9294fd
ignacio <ignacio.alvaradoa@sansano.usm.cl>
2024-06-13 01:40:49 -0400

    chore: validation for medic creation

--- frontend/src/pages/newSpeciality.jsx
+++ frontend/src/pages/newSpeciality.jsx
@@ -5,6 +5,7 @@ import { Card } from "primereact/card";
 import { InputText } from "primereact/inputtext";
 import { Link, useNavigate } from "react-router-dom";
 import { backendUrl } from "../config/backend-url";
+import { FloatLabel } from "primereact/floatlabel";
 
 const NewSpeciality = () => {
   const navigate = useNavigate();
@@ -60,23 +61,28 @@ const NewSpeciality = () => {
             className="px-4 w-1"
             icon="pi pi-home"
             size="large"
+            text
+            plain
           />
         </Link>
       </div>
       <h2 className="mt-8 ml-5">Crear especialidad</h2>
-      <Card>
+      <Card className="pl-8">
         <form onSubmit={formik.handleSubmit}>
-          <label htmlFor="speciality" className="block">
-            Nombre
-          </label>
-          <InputText
-            id="speciality"
-            value={formik.values.speciality}
-            onChange={(e) =>
-              formik.setFieldValue('speciality', e.target.value)
-            }
-          />
-          <small className="text-red-500">{formik.errors.speciality}</small>
+          <h3 className="text-left ml-8 my-5">Especialidad</h3>
+          <FloatLabel>
+            <InputText
+              id="speciality"
+              key="speciality"
+              className="block"
+              value={formik.values.speciality}
+              onChange={(e) =>
+                formik.setFieldValue('speciality', e.target.value)
+              }
+            />
+            <label htmlFor="speciality">Ingrese especialidad</label>
+          </FloatLabel>
+          <small className="text-red-500 block text-left mt-2 ml-2">{formik.errors.speciality}</small>
           <Button
             type="submit"
             label="Crear especialidad"
